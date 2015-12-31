4e68d7ec03b6a0380e8e8a5a159e1f14719791c0
kkk359641513 <359641513@qq.com>
2015-12-31 15:07:47 +0800

    Initial Commit

---
+++ ShopDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShopDemo
+//
+//  Created by kkk on 15/12/31.
+//  Copyright © 2015年 kkk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShopDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShopDemo
+//
+//  Created by kkk on 15/12/31.
+//  Copyright © 2015年 kkk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
